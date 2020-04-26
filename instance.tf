a6f74183f6714bff74c4b44a4e0fa863a14d1c9a
EC2 Default User <ec2-user@ip-172-31-28-51.eu-west-2.compute.internal>
2020-04-26 03:22:39 +0000

    created

--- instance.tf
+++ instance.tf
@@ -7,3 +7,14 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+# resource "aws_volume_attachment" "ebs_att" {
+#   device_name = "/dev/sdh"
+#   volume_id   = "${aws_ebs_volume.example.id}"
+#   instance_id = "${aws_instance.web.id}"
+# }
+
+# resource "aws_ebs_volume" "example" {
+#   region = "us-west-2"
+#   size              = 100G
+# }
