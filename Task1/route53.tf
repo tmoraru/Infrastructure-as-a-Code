07689ff809110d718d679975991b03052da58e26
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-04-27 13:10:29 -0500

    Delete route53.tf

--- Task1/route53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "route53" {
-  zone_id = "Z2C2TTKT34Z4V4"
-  name    = "www.tatianamoraru.com"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.web.public_ip}"]
-}
