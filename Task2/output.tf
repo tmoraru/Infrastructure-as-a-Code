134f8c2b89f35591976881db213dd4550cf2cc9d
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-04-28 08:34:39 -0500

    Update output.tf

--- Task2/output.tf
+++ Task2/output.tf
@@ -1,1 +1,32 @@
+output "key_pair" {
+  value       =  "${aws_key_pair.us-east-1-key-output.key_name}"
+  description = ""
+}
 
+output "instance_id" {
+  value       = "${aws_instance.web-output.id}"
+  description = ""
+}
+
+output "instance_ip" {
+  value       = "${aws_instance.web-output.public_ip}"
+  
+}
+
+output "instance_arn" {
+  value       = "${aws_instance.web-output.arn}"
+  
+}
+
+output "public_dns" {
+  value       = "${aws_instance.web-output.public_dns}"
+  
+}
+output "route53" {
+  value = "${aws_route53_record.www.name}"
+}
+
+output "message" {
+  value       = "Login to wordpress and reset passwd"
+  
+}
