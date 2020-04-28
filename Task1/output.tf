f82c3416e8718c2c9e02a71d22067717e997d58f
Taniusellu <tatianamoraru1998@gmail.com>
2020-04-28 02:51:27 +0000

    added

---
+++ Task1/output.tf
@@ -0,0 +1,22 @@
+output "IP" {
+  value = "${aws_instance.web.public_ip}"
+}
+
+output "KEY" {
+  value = "${aws_instance.web.key_name}"
+}
+
+
+output "Sec_Group" {
+  value = "${aws_security_group.allow_tls.name}"
+}
+
+output "AMI" {
+  value = "${aws_instance.web.ami}"
+}
+
+output "ROUTE53" {
+  value = "${aws_route53_record.www.name}"
+}
+
+
--- output_folder/provider.tf
+++ output_folder/provider.tf
@@ -15,8 +15,54 @@ resource "aws_instance" "web-output" {
 }
 // Create Sec Group
 
-// Create Route53
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+// Create Route53
+resource "aws_route53_record" "www" {
+  zone_id = "Z2C2TTKT34Z4V4"
+  name    = "www.tatianamoraru.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_instance.web-output.public_ip}"]
+}
 // Outputs
 
 output "key_pair" {
@@ -28,3 +74,27 @@ output "instance_id" {
   value       = "${aws_instance.web-output.id}"
   description = ""
 }
+
+output "instance_ip" {
+  value       = "${aws_instance.web-output.public_ip}"
+  
+}
+
+output "instance_arn" {
+  value       = "${aws_instance.web-output.arn}"
+  
+}
+
+output "public_dns" {
+  value       = "${aws_instance.web-output.public_dns}"
+  
+}
+output "route53" {
+  value = "${aws_route53_record.www.name}"
+}
+
+output "message" {
+  value       = "Login to wordpress and reset passwd"
+  
+}
+
