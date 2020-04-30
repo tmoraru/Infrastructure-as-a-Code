7e4570d3ede3e70c54156264d88a9b4e8959a5b8
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-04-30 14:25:32 -0500

    Update route-table.tf

--- Task3/route-table.tf
+++ Task3/route-table.tf
@@ -6,12 +6,7 @@ resource "aws_route_table" "dev" {
     gateway_id = "${aws_internet_gateway.dev.id}"
    
   }
-   tags =  {
-    Environment = "${var.Environment}"
-      Department = "${var.Department}"
-      Team = "${var.Team}"
-      Created_by = "${var.Created_by}"
-   }
+   tags = "${var.tags}"
 }
  
 #Attached from subnet association
