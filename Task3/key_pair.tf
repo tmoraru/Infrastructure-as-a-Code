4167bab439d0aa2d843ca686eb359e6c1520685d
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-04-30 14:24:51 -0500

    Update key_pair.tf

--- Task3/key_pair.tf
+++ Task3/key_pair.tf
@@ -1,10 +1,5 @@
 resource "aws_key_pair" "us-west-2-key" {
   key_name   = "tatiana_bastion-2"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
- tags =  {
-    Environment = "${var.Environment}"
-      Department = "${var.Department}"
-      Team = "${var.Team}"
-      Created_by = "${var.Created_by}"
-   }
+tags = "${var.tags}"
 } 
