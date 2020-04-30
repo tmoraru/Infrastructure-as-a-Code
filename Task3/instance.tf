ba3f0f3125063f3233999400f60b9b0d37c8cd5c
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-04-30 10:00:02 -0500

    Create instance.tf

---
+++ Task3/instance.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "dev" {
+  ami           = "ami-0d6621c01e8c2de2c"
+  instance_type = "t2.micro"
+  key_name      =     "${aws_key_pair.us-west-2-key.key_name}"
+  security_groups = ["${aws_security_group.allow_tls.name}"]
+  user_data = "${file("user_data.sh")}"
+  availability_zone = "us-west-2a"
+ tags =  {
+    Environment = "${var.Environment}"
+      Department = "${var.Department}"
+      Team = "${var.Team}"
+      Created_by = "${var.Created_by}"
+   }
+  
+}
