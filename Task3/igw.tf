176b13a40ee70b76d731ec561f1966902ca11a79
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-04-30 14:24:21 -0500

    Update igw.tf

--- Task3/igw.tf
+++ Task3/igw.tf
@@ -1,9 +1,4 @@
 resource "aws_internet_gateway" "dev" {
   vpc_id = "${aws_vpc.dev.id}"
- tags =  {
-    Environment = "${var.Environment}"
-      Department = "${var.Department}"
-      Team = "${var.Team}"
-      Created_by = "${var.Created_by}"
-   }
+ tags = "${var.tags}"
 }
