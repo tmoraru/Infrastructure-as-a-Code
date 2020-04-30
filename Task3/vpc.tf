ad8ab4b3b7910b4135b66fcde5e68f5676cf9346
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-04-30 14:27:32 -0500

    Update vpc.tf

--- Task3/vpc.tf
+++ Task3/vpc.tf
@@ -4,10 +4,5 @@ resource "aws_vpc" "dev" {
  cidr_block = "${var.cidr_block}"
 
 
-tags =  {
-    Environment = "${var.Environment}"
-      Department = "${var.Department}"
-      Team = "${var.Team}"
-      Created_by = "${var.Created_by}"
-   }
+tags = "${var.tags}"
 }
