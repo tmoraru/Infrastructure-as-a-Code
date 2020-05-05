b802c7df042fb57f6915b736e58066372909627a
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-05-05 09:47:47 -0500

    Update output.tf

--- Jenkins/output.tf
+++ Jenkins/output.tf
@@ -1,31 +1,31 @@
 output "vpc" {
   value = [
-    "${aws_vpc.main.id}        ${aws_vpc.main.cidr_block}"
+    "${aws_vpc.dev.id}        ${aws_vpc.dev.cidr_block}"
   ]
 }
 
 output "public_subnets" {
   value = [
-    "${aws_subnet.public_subnet1.id}     ${aws_subnet.public_subnet1.cidr_block}     ${aws_subnet.public_subnet1.availability_zone}" ,
-    "${aws_subnet.public_subnet2.id}     ${aws_subnet.public_subnet2.cidr_block}     ${aws_subnet.public_subnet2.availability_zone}" ,  
-    "${aws_subnet.public_subnet3.id}     ${aws_subnet.public_subnet3.cidr_block}     ${aws_subnet.public_subnet3.availability_zone}"
+    "${aws_subnet.dev1.id}     ${aws_subnet.dev1.cidr_block}     ${aws_subnet.dev1.availability_zone}" ,
+    "${aws_subnet.dev2.id}     ${aws_subnet.dev2.cidr_block}     ${aws_subnet.dev2.availability_zone}" ,  
+    "${aws_subnet.dev3.id}     ${aws_subnet.dev3.cidr_block}     ${aws_subnet.dev3.availability_zone}"
   ] 
 }
 
 output "private_subnets" {
   value = [
-    "${aws_subnet.private_subnet1.id}     ${aws_subnet.private_subnet1.cidr_block}     ${aws_subnet.private_subnet1.availability_zone}",
-    "${aws_subnet.private_subnet2.id}     ${aws_subnet.private_subnet2.cidr_block}     ${aws_subnet.private_subnet2.availability_zone}",
-    "${aws_subnet.private_subnet3.id}     ${aws_subnet.private_subnet3.cidr_block}     ${aws_subnet.private_subnet3.availability_zone}"
+    "${aws_subnet.dev_private1.id}     ${aws_subnet.dev_private1.cidr_block}     ${aws_subnet.dev_private1.availability_zone}",
+    "${aws_subnet.dev_private2.id}     ${aws_subnet.dev_private2.cidr_block}     ${aws_subnet.dev_private2.availability_zone}",
+    "${aws_subnet.dev_private3.id}     ${aws_subnet.dev_private3.cidr_block}     ${aws_subnet.dev_private3.availability_zone}"
   ]
 }
 
 output "igw_id" {
-  value = "${aws_internet_gateway.gw.id}    ${aws_route_table.rt_gw.id}"
+  value = "${aws_internet_gateway.dev.id}   
 }
 
 output "ngw_id" {
-  value = "${aws_nat_gateway.ngw.id}        ${aws_route_table.rt_ngw.id}"
+   value = "${aws_nat_gateway.ngw.id}        
 }
 
 output "region" {
