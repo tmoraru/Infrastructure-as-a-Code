8f27f5bc42ffb7053f1a7c5e71306ed47cc91650
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-05-06 11:56:27 -0500

    Create sec_group_bastion.tf

---
+++ VPC_Peering/sec_group_bastion.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_tls_bastion" {
+  name        = "allow_tls2"
+  description = "Allow TLS inbound traffic"
+  vpc_id = "${aws_vpc.dev.id}"
+  
+
+  ingress {  
+    description = "TLS from VPC"
+    from_port   = 22 
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+ 
+  egress {    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags ="${var.tags}"
+}
