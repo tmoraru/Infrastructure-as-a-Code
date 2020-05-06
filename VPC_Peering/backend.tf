1b93379e9b7a53d0b78280ab2c1e6f12f62a1cfb
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-05-06 12:00:10 -0500

    Create backend.tf

---
+++ VPC_Peering/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+    required_version = "0.11.14"
+  backend "s3" {
+    bucket = "tatiana-state-bucket"
+    key    = "project/us-east-1/tools/virginia/project.tfstate"
+    region = "us-east-1"
+  }
+}
