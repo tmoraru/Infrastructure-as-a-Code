0892d8682ba9b352e065b0ca8d75ce9c9acaa1b7
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-11 22:17:43 +0000

    created

--- AWS/app/asg_module.tf
+++ AWS/app/asg_module.tf
@@ -1,7 +1,15 @@
 module "app1" { 
-
 source = "./module" 
-
+region = "us-east-1"
 } 
 
-variable "region" {}
+
+module "wordpress" { 
+source = "./module" 
+region = "us-east-1"
+} 
+
+module "wordpress-london" { 
+source = "./module" 
+region = "eu-west-2"
+} 
