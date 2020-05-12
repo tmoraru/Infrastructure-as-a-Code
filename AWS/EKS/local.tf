7838638213981cec8ba56136bd04156ce449d5e8
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-12 19:17:00 +0000

    added

--- AWS/EKS/local.tf
+++ AWS/EKS/local.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "login" {
     provisioner "local-exec" {
-    command = "aws eks --region var.region update-kubeconfig --name var.cluster_name"
+    command = "aws eks --region ${var.region} update-kubeconfig --name ${var.cluster_name}"
     }
     provisioner "local-exec" {
     command = "kubectl get nodes"
