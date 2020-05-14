e85b83c24543a5d610bb229f686534bec64b8408
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-14 20:55:28 +0000

    added

--- AWS/EKS/backend.tf
+++ AWS/EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform { 
 backend "s3" { 
 bucket = "taniusha-bucket-eks" 
-key = "application/us-east-1/state/stage/infrastructure.tfstate_london.json" 
+key = "application/us-east-1/state/dev/infrastructure.tfstate.json" 
 region = "us-east-1" 
   } 
 } 
