c60029196f0503f1a304fad25f9ed208675654c0
Tatiana Moraru <tatianamoraru1998@gmail.com>
2021-02-24 07:51:06 -0600

    Update sec_group.tf

--- sec_group.tf
+++ sec_group.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls_february"
   description = "Allow TLS inbound traffic"
-
+   
   ingress {
     description = "TLS from VPC"
     from_port   = 443
