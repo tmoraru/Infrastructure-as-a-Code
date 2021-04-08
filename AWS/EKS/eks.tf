a18f5a0e95d151e28f746469fccf1413facfb038
Tatiana Moraru <tatianamoraru1998@gmail.com>
2021-04-08 12:14:30 -0500

    Update eks.tf

--- AWS/EKS/eks.tf
+++ AWS/EKS/eks.tf
@@ -1,7 +1,7 @@
 data "aws_eks_cluster" "cluster" {
   name = module.my-cluster.cluster_id
 }
-data "aws_eks_cluster_auth" "cluster" {
+data "aws_eks_cluster_auth" "cluster" { 
   name = module.my-cluster.cluster_id
 }
 
