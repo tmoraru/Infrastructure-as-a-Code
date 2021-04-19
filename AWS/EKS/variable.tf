e08660dda1b10ea103f3d31044bf040225aefdf9
Tatiana Moraru <tatianamoraru1998@gmail.com>
2021-04-18 20:46:22 -0500

    Update variable.tf

--- AWS/EKS/variable.tf
+++ AWS/EKS/variable.tf
@@ -4,7 +4,7 @@ variable "subnet2" {}
 variable "subnet3" {}
 variable "cluster_name" {}
 variable "environment" {}
-variable "instance_type" {}
+variable "instance_type" {} 
 variable "asg_max_size" {}
 variable "asg_min_size" {}
 variable "cluster_version" {}
