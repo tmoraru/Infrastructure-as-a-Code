7dad4adb4bb931c407d9b67489f79b1de75c5ad6
Tatiana Moraru <tatianamoraru1998@gmail.com>
2021-04-25 21:23:33 -0500

    Update sec_group.tf

--- sec_group.tf
+++ sec_group.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls_february" 
+  name        = "allow_tls_february"  
   description = "Allow TLS inbound traffic"
    
   ingress {
