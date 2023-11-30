4d90e774955b51ad7f8ee4e567ad56b1de03bd64
Tatiana Moraru <tatianamoraru1998@gmail.com>
2023-11-29 21:11:57 -0600

    Update asg.tf

--- AWS/ASG/2-ASG/asg.tf
+++ AWS/ASG/2-ASG/asg.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_configuration" "as_conf" {
-  name          = "web_config"
+  name          = "web_config" 
   image_id      = "${data.aws_ami.image.id}"
   instance_type = "t2.micro"
   user_data     = "${file("wordpress.sh")}"
